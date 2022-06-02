6dbfefff851f1f09ea48f812218bb9fd2d71897a
tenzinmigmar <70817213+tenzinmigmar@users.noreply.github.com>
2022-06-01 22:17:39 -0400

    added vectors.py

---
+++ src/vectors.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+class Vector:
+    def __init__(self, u, v, color, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.u = u
+        self.v = v
+        self.color = color
