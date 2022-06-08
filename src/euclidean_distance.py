43a7c0d1f3f4ed32d549d53b6ee781e734dbe4e2
tenzinmigmar <tenzin.migmarr@gmail.com>
2022-06-08 14:53:15 -0400

    Added euclidean_distance.py

---
+++ src/euclidean_distance.py
@@ -0,0 +1,12 @@
+"""
+Euclidean distance is distance
+between two points in Euclidean
+space.
+"""
+import math
+
+def euclidean_distance(p, q):
+    assert isinstance(p, tuple)
+    assert isinstance(q, tuple)
+
+euclidean_distance((3, 4), (7, 2))
