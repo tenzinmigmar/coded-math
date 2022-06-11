fb586e1b8701845d889834759b94fc64dce898d6
tenzinmigmar <70817213+tenzinmigmar@users.noreply.github.com>
2022-06-11 15:30:16 -0400

    edited euclidean_distance.py

--- src/euclidean_distance.py
+++ src/euclidean_distance.py
@@ -5,8 +5,19 @@ space.
 """
 import math
 
-def euclidean_distance(p, q):
-    assert isinstance(p, tuple)
-    assert isinstance(q, tuple)
 
-euclidean_distance((3, 4), (7, 2))
+def euclidean_distance(p, q):
+    assert isinstance(p, tuple) or isinstance(p, list)
+    assert len(p) == 2
+    assert isinstance(q, tuple) or isinstance(q, list)
+    assert len(q) == 2
+
+    euclid_distance = math.sqrt((q[0] - p[0]) ** 2 + (q[1] - p[1]) ** 2)
+    print(euclid_distance)
+
+
+# Should print 4
+euclidean_distance((1, 3), (1, -1))
+
+# Should print 4
+euclidean_distance((1, -1), (5, -1))
